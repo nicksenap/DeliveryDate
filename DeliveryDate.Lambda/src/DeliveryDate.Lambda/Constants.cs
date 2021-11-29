f706c293592ace3071c14c72ca74a87c0bef46ba
Nick Song <nick.senap@gmail.com>
2021-11-29 20:03:35 +0100

    restructuring the repo.

--- DeliveryDate.Lambda/src/DeliveryDate.Lambda/Constants.cs
+++
@@ -1,8 +0,0 @@
-namespace DeliveryDate.Lambda
-{
-    public static class Constants
-    {
-        public const int TWO_WEEKS = 14;
-        public const int EXTERNAL_PRODUCT_DAYS_IN_ADVANCE = 5;
-    }
-}
--- DeliveryDate.Lambda/src/DeliveryDate.Lambda/Enums/ProductType.cs
+++
@@ -1,9 +0,0 @@
-namespace DeliveryDate.Lambda.Enums
-{
-    public enum ProductType
-    {
-        Normal = 1,
-        External = 2,
-        Temporary = 3
-    }
-}
